f87c0f0e243628f46f9f2de99b70854e3f6f7e37
tankhanhdao <tankhanhdao@gmail.com>
2022-07-24 08:10:19 -0700

    addded .env file 7/24/2022

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,8 +23,7 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5=*+ek3+)(k6zth-y#b)i7#9ao_f+2a@jrdx*xihq6hanb@j!x'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
